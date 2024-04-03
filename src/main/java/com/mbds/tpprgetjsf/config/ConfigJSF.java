7fe8bec41dbc171a9269ba13275485986960f3bb
rotsyrotsy <80813050+rotsyrotsy@users.noreply.github.com>
2024-04-03 21:30:33 +0300

    Creation du projet

---
+++ src/main/java/com/mbds/tpprgetjsf/config/ConfigJSF.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mbds.tpprgetjsf.config;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.faces.annotation.FacesConfig;
+
+/**
+ * Configuration JSF
+ *
+ * @author Rotsy
+ */
+@ApplicationScoped
+@FacesConfig
+public class ConfigJSF {
+
+}
