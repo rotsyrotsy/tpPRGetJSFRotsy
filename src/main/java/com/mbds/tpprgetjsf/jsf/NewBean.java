b21ac5f60a2f959615ed72cbc6d82b9ece596d6f
rotsyrotsy <80813050+rotsyrotsy@users.noreply.github.com>
2024-04-11 22:24:43 +0300

    TP3 : Création des clients du template et remise en forme des contenus des pages

--- src/main/java/com/mbds/tpprgetjsf/jsf/NewBean.java
+++ src/main/java/com/mbds/tpprgetjsf/jsf/NewBean.java
@@ -40,5 +40,8 @@ public class NewBean {
     public String afficher() {
         return "affichage_4?nb=" + nombre + "&amp;faces-redirect=true";
     }
+    public String afficherExercice4() {
+        return "affichage__4?nb=" + nombre + "&amp;faces-redirect=true";
+    }
 
 }
